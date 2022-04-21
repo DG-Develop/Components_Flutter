9e287928fb58bbbbe57f1445d0c9982233b68379
DG-Develop <dgdband@gmail.com>
2022-04-20 23:27:14 -0500

    [ADD] Agregnado un custom widget para los inputsfield para el formulario para que sea dinamico

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_components/widgets/custom_input_field.dart';
 
 class InputsScreen extends StatelessWidget {
   const InputsScreen({Key? key}) : super(key: key);
@@ -10,33 +11,36 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(children: [
-          TextFormField(
-            autofocus: false,
-            initialValue: '',
-            textCapitalization: TextCapitalization.words,
-            onChanged: (value) {
-              print('value $value');
-            },
-            validator: (value) {
-              if (value == null) return 'Este campo es requerido';
-              return value.length < 3 ? 'Minimo de 3 letras' : null;
-            },
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            decoration: const InputDecoration(
-                hintText: 'Nombre del usuario',
-                labelText: 'Nombre',
-                helperText: 'Solo letras',
-                // prefixIcon: Icon(Icons.verified_user_outlined),
-                suffixIcon: Icon(Icons.group_outlined),
-                icon: Icon(Icons.assignment_ind_outlined),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        topRight: Radius.circular(10)))),
+        child: Column(children: const [
+          CustomInputField(
+            labelText: 'Nombre',
+            hintText: 'Nombre de Usuario',
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          CustomInputField(
+            labelText: 'Apellido',
+            hintText: 'Apellido del usuario',
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          CustomInputField(
+            labelText: 'Correo',
+            hintText: 'Correo del usuario',
+            keyboardType: TextInputType.emailAddress,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          CustomInputField(
+            labelText: 'Contraseña',
+            hintText: 'Contraseña del usuario',
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 30,
           )
         ]),
       )),
